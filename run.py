9954befcc302160745472667c356c0f576e2b851
Alejandro Albaladejo Gago <alealbaladejo29s@gmail.com>
2025-05-03 14:39:40 +0200

    run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import os
 from app.routes import app
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Obtiene el puerto de la variable de entorno
-    app.run(host="0.0.0.0", port=port)  # Escucha en todas las interfaces de red
+    port = int(os.getenv("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)  
